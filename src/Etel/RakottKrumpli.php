be6016a9fa599f8db75e9db40afe3a241d3cd544
ypszi <ypszi93@gmail.com>
2024-03-25 15:36:30 +0100

    feat: RakottKrumpli

---
+++ src/Etel/RakottKrumpli.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PeterPecosz\Kajatervezo\Etel;
+
+use PeterPecosz\Kajatervezo\Hozzavalo\FuszerEsOlaj\So;
+use PeterPecosz\Kajatervezo\Hozzavalo\Hus\BaconKockazott;
+use PeterPecosz\Kajatervezo\Hozzavalo\Hus\Kolbasz;
+use PeterPecosz\Kajatervezo\Hozzavalo\Hutos\Tejfol;
+use PeterPecosz\Kajatervezo\Hozzavalo\Hutos\Tojas;
+use PeterPecosz\Kajatervezo\Hozzavalo\HutosUtan\Vaj;
+use PeterPecosz\Kajatervezo\Hozzavalo\Zoldseg\Krumpli;
+use PeterPecosz\Kajatervezo\Mertekegyseg\Mertekegyseg;
+
+class RakottKrumpli extends Etel
+{
+    #[\Override] public static function name(): string
+    {
+        return 'Rakott krumpli';
+    }
+
+    #[\Override] protected static function listHozzavalok(): array
+    {
+        return [
+            new Krumpli(1, Mertekegyseg::KG),
+            new Tojas(6, Mertekegyseg::DB),
+            new So(1, Mertekegyseg::TK),
+            new Kolbasz(10, Mertekegyseg::DKG),
+            new Tejfol(5, Mertekegyseg::DL),
+            new Vaj(2, Mertekegyseg::TK),
+            new BaconKockazott(10, Mertekegyseg::DKG),
+        ];
+    }
+
+    #[\Override] public static function defaultAdag(): int
+    {
+        return 4;
+    }
+
+    #[\Override] public function receptUrl(): string
+    {
+        return 'https://www.mindmegette.hu/rakott-krumpli.recept/';
+    }
+}
--- tests/Etel/Factory/EtelFactoryTest.php
+++ tests/Etel/Factory/EtelFactoryTest.php
@@ -30,6 +30,6 @@ class EtelFactoryTest extends TestCase
     #[Test]
     public function testListAvailableEtelek(): void
     {
-        $this->assertCount(20, EtelFactory::listAvailableEtelek());
+        $this->assertCount(21, EtelFactory::listAvailableEtelek());
     }
 }
