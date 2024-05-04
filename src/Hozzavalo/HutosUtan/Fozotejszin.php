25ab2d21bcef668f8ad896aa5b50ec9f419cea1c
ypszi <ypszi93@gmail.com>
2024-05-04 16:11:22 +0200

    fix: Fozotejszin mertekegyseg preference to ml

--- src/Hozzavalo/HutosUtan/Fozotejszin.php
+++ src/Hozzavalo/HutosUtan/Fozotejszin.php
@@ -4,10 +4,17 @@ declare(strict_types=1);
 
 namespace PeterPecosz\Kajatervezo\Hozzavalo\HutosUtan;
 
+use PeterPecosz\Kajatervezo\Mertekegyseg\Mertekegyseg;
+
 class Fozotejszin extends HutosUtan
 {
     #[\Override] public static function name(): string
     {
         return 'Főzőtejszín';
+    }
+
+    #[\Override] public static function mertekegysegPreference(): ?string
+    {
+        return Mertekegyseg::ML;
     }
 }
