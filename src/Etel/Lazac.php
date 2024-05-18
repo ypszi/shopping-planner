d0a15324e024f0b9321fdd31ad7befff3d79b51d
ypszi <ypszi93@gmail.com>
2024-05-18 11:07:31 +0200

    feat: Lazac etel

---
+++ src/Etel/Lazac.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PeterPecosz\Kajatervezo\Etel;
+
+use PeterPecosz\Kajatervezo\Hozzavalo\FuszerEsOlaj\Bors;
+use PeterPecosz\Kajatervezo\Hozzavalo\FuszerEsOlaj\Kakukkfu;
+use PeterPecosz\Kajatervezo\Hozzavalo\FuszerEsOlaj\NapraforgoOlaj;
+use PeterPecosz\Kajatervezo\Hozzavalo\FuszerEsOlaj\Rozmaring;
+use PeterPecosz\Kajatervezo\Hozzavalo\FuszerEsOlaj\So;
+use PeterPecosz\Kajatervezo\Hozzavalo\Hus\Lazac as NyersLazac;
+use PeterPecosz\Kajatervezo\Hozzavalo\HutosUtan\Vaj;
+use PeterPecosz\Kajatervezo\Mertekegyseg\Mertekegyseg;
+
+class Lazac extends Etel
+{
+    #[\Override] public static function name(): string
+    {
+        return 'Lazac';
+    }
+
+    #[\Override] protected static function listHozzavalok(): array
+    {
+        return [
+            new NyersLazac(36, Mertekegyseg::DKG),
+            new So(2, Mertekegyseg::CSIPET),
+            new Bors(1, Mertekegyseg::CSIPET),
+            new Rozmaring(2, Mertekegyseg::CSIPET),
+            new Kakukkfu(2, Mertekegyseg::CSIPET),
+            new NapraforgoOlaj(2, Mertekegyseg::EK),
+            new Vaj(1, Mertekegyseg::EK),
+        ];
+    }
+
+    #[\Override] public static function defaultAdag(): int
+    {
+        return 2;
+    }
+
+    #[\Override] public function receptUrl(): string
+    {
+        return $this->decorateNoSaltyReceptUrl('https://www.nosalty.hu/recept/lazac-egyszeruen');
+    }
+}
--- tests/Etel/Factory/EtelFactoryTest.php
+++ tests/Etel/Factory/EtelFactoryTest.php
@@ -30,6 +30,6 @@ class EtelFactoryTest extends TestCase
     #[Test]
     public function testListAvailableEtelek(): void
     {
-        $this->assertCount(43, EtelFactory::listAvailableEtelek());
+        $this->assertCount(44, EtelFactory::listAvailableEtelek());
     }
 }
