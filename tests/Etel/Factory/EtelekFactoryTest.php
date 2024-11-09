d45ac810403f9ad748875fe3b71c42317d9b9073
ypszi <ypszi93@gmail.com>
2024-11-09 14:22:22 +0100

    feat(foods): Görög joghurtos torta

--- tests/Etel/Factory/EtelekFactoryTest.php
+++ tests/Etel/Factory/EtelekFactoryTest.php
@@ -122,6 +122,6 @@ class EtelekFactoryTest extends TestCase
 
     public function testCreateAvailableFoods(): void
     {
-        $this->assertCount(75, $this->sut->listAvailableFoods());
+        $this->assertCount(76, $this->sut->listAvailableFoods());
     }
 }
