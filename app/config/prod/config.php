aba065e5684c8e4841d15593e9af5b3e3a0bbe4b
ypszi <ypszi93@gmail.com>
2024-11-16 09:47:04 +0100

    feat: prod route, container, twig cache turned back on

--- app/config/prod/config.php
+++ app/config/prod/config.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 return [
     'settings.display_error_details' => true,
 
-    'cache.routes_cache.is_enabled'    => false,
-    'cache.container_cache.is_enabled' => false,
+    'cache.routes_cache.is_enabled'    => true,
+    'cache.container_cache.is_enabled' => true,
 
     'twig.parameters' => [
-//        'cache' => __DIR__ . '/../../../var/cache/twig/',
+        'cache' => __DIR__ . '/../../../var/cache/twig/',
     ],
 ];
