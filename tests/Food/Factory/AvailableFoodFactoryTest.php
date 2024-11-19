2791c9213d0c407a1f56d80596832ae1735953f9
ypszi <ypszi93@gmail.com>
2024-11-19 08:41:10 +0100

    feat(foods): Sertéspörkölt

--- tests/Food/Factory/AvailableFoodFactoryTest.php
+++ tests/Food/Factory/AvailableFoodFactoryTest.php
@@ -30,7 +30,7 @@ class AvailableFoodFactoryTest extends TestCase
     #[Test]
     public function testListAvailableFoods(): void
     {
-        $this->assertCount(84, $this->sut->listAvailableFoods(new FoodFilterInput()));
+        $this->assertCount(85, $this->sut->listAvailableFoods(new FoodFilterInput()));
     }
 
     #[Test]
@@ -48,7 +48,7 @@ class AvailableFoodFactoryTest extends TestCase
     {
         return [
             'reggeli' => ['tags' => ['reggeli'], 'expectedCount' => 0],
-            'ebéd'    => ['tags' => ['ebéd'], 'expectedCount' => 3],
+            'ebéd'    => ['tags' => ['ebéd'], 'expectedCount' => 4],
             'vacsora' => ['tags' => ['vacsora'], 'expectedCount' => 4],
             'köret'   => ['tags' => ['köret'], 'expectedCount' => 2],
             'saláta'  => ['tags' => ['saláta'], 'expectedCount' => 6],
@@ -61,7 +61,7 @@ class AvailableFoodFactoryTest extends TestCase
     public function testListAvailableFoodsFilteredForMultipleTags(): void
     {
         $this->assertCount(
-            5,
+            6,
             $this->sut->listAvailableFoods(new FoodFilterInput(tags: ['reggeli', 'ebéd', 'vacsora'])),
             'Expectation failed for tags: "reggeli, ebéd, vacsora"'
         );
