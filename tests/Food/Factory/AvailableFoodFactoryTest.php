580c6e45f8172d9208897ad307c47dfaaab0cd0b
ypszi <ypszi93@gmail.com>
2025-02-28 20:01:39 +0100

    feat(foods): Vajas kukoricás csicseriborsó

--- tests/Food/Factory/AvailableFoodFactoryTest.php
+++ tests/Food/Factory/AvailableFoodFactoryTest.php
@@ -31,7 +31,7 @@ class AvailableFoodFactoryTest extends TestCase
     #[Test]
     public function testListAvailableFoods(): void
     {
-        $this->assertCount(111, $this->sut->listAvailableFoods(new FoodFilterInput()));
+        $this->assertCount(112, $this->sut->listAvailableFoods(new FoodFilterInput()));
     }
 
     #[Test]
@@ -51,11 +51,11 @@ class AvailableFoodFactoryTest extends TestCase
             'reggeli'  => ['tags' => ['reggeli'], 'expectedCount' => 3],
             'ebéd'     => ['tags' => ['ebéd'], 'expectedCount' => 59],
             'vacsora'  => ['tags' => ['vacsora'], 'expectedCount' => 26],
-            'köret'    => ['tags' => ['köret'], 'expectedCount' => 13],
+            'köret'    => ['tags' => ['köret'], 'expectedCount' => 14],
             'leves'    => ['tags' => ['leves'], 'expectedCount' => 4],
             'saláta'   => ['tags' => ['saláta'], 'expectedCount' => 19],
             'desszert' => ['tags' => ['desszert'], 'expectedCount' => 16],
-            'új'       => ['tags' => ['új'], 'expectedCount' => 14],
+            'új'       => ['tags' => ['új'], 'expectedCount' => 17],
             'suli'     => ['tags' => ['suli'], 'expectedCount' => 5],
             'ital'     => ['tags' => ['ital'], 'expectedCount' => 1],
         ];
